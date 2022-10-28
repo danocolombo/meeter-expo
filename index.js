c0165c90bf85a2606e97200fb3678080f98a0d7d
Dano Colombo <danocolombo@gmail.com>
2022-10-27 20:15:07 -0400

    going to expo

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 import { registerRootComponent } from 'expo';
 
-import awsconfig from './src/aws-exports';
-import { Amplify } from 'aws-amplify';
-Amplify.configure({
-    ...awsconfig,
-    Analytics: {
-        disabled: true,
-    },
-});
 import App from './App';
 registerRootComponent(App);

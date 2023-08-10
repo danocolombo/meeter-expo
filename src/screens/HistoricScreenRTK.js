3daad9685dff1161afdbfaa65bea0d511e9a02b6
Dano <danocolombo@gmail.com>
2023-08-10 17:04:55 -0400

    Historic screen styled

--- src/screens/HistoricScreenRTK.js
+++ src/screens/HistoricScreenRTK.js
@@ -1,29 +1,10 @@
-import {
-    StyleSheet,
-    Text,
-    View,
-    FlatList,
-    ImageBackground,
-} from 'react-native';
-import React, {
-    useLayoutEffect,
-    useEffect,
-    useState,
-    useCallback,
-} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useState, useCallback } from 'react';
 import { Surface, useTheme, ActivityIndicator, FAB } from 'react-native-paper';
-import {
-    useNavigation,
-    useFocusEffect,
-    useNavigationState,
-} from '@react-navigation/native';
-import { useSelector, useDispatch } from 'react-redux';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { useDispatch } from 'react-redux';
 import MeetingListCard from '../components/Meeting.List.Card';
-import {
-    printObject,
-    getDateMinusDays,
-    createMtgCompKey,
-} from '../utils/helpers';
+import { printObject } from '../utils/helpers';
 import { getHistoricMeetings } from '../features/meetings/meetingsThunks';
 const HistoricScreen = (props) => {
     const navigation = useNavigation();
@@ -51,20 +32,12 @@ const HistoricScreen = (props) => {
                 });
         }, [])
     );
-    const handleNewRequest = () => {
-        navigation.navigate('MeetingNew');
-    };
+
     if (isLoading) {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
+            <View style={mtrStyles(mtrTheme).activityIndicatorContainer}>
                 <ActivityIndicator
-                    color={mtrTheme.colors.activityIndicator}
+                    color={mtrStyles(mtrTheme).activityIndicator}
                     size={80}
                 />
             </View>
@@ -73,12 +46,12 @@ const HistoricScreen = (props) => {
 
     return (
         <>
-            <Surface style={mtrTheme.screenSurface}>
-                <View style={styles.screenHeader}>
-                    <Text style={styles.screenHeaderText}>
+            <Surface style={mtrStyles(mtrTheme).screenSurface}>
+                <View style={mtrStyles(mtrTheme).screenHeader}>
+                    <Text style={mtrStyles(mtrTheme).screenHeaderText}>
                         Historic Meetings
                     </Text>
-                    <Text style={mtrTheme.subTitleSmall}>
+                    <Text style={mtrStyles(mtrTheme).subTitleSmall}>
                         Click event for details!
                     </Text>
                 </View>
@@ -104,22 +77,36 @@ const HistoricScreen = (props) => {
 };
 
 export default HistoricScreen;
-
-const styles = StyleSheet.create({
-    rootContainer: {
-        flex: 1,
-    },
-    screenHeader: {
-        alignItems: 'center',
-    },
-    screenHeaderText: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        color: 'white',
-    },
-    bgImageContainer: {
-        flex: 1,
-        height: '100%',
-        width: '100%',
-    },
-});
+const mtrStyles = (mtrTheme) =>
+    StyleSheet.create({
+        screenSurface: {
+            flex: 1,
+            backgroundColor: mtrTheme.colors.background,
+        },
+        activityIndicatorContainer: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        activityIndicator: {
+            color: mtrTheme.colors.lightGraphic,
+        },
+        screenHeader: {
+            marginTop: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        screenHeaderText: {
+            fontSize: 30,
+            fontFamily: 'Roboto-Bold',
+            color: mtrTheme.colors.lightText,
+        },
+        subTitleSmall: {
+            fontFamily: 'Roboto-Medium',
+            fontSize: 16,
+            fontWeight: '500',
+            color: mtrTheme.colors.accent,
+            textAlign: 'center',
+            paddingBottom: 5,
+        },
+    });

12e6025d0e463fbd463424eb353fd5d85bfc8656
Dano <danocolombo@gmail.com>
2023-09-30 19:21:17 -0400

    new branch

--- app.config.js
+++ app.config.js
@@ -1,8 +1,8 @@
 module.exports = ({ config }) => {
-    let BASE_version = '1.0.25';
+    let BASE_version = '1.0.26';
     let ANDROID_version = 1;
     let COMBO_version = BASE_version + '-' + ANDROID_version.toString();
-    let DESCRIPTION = 'expo/1.1.0 - sprint25';
+    let DESCRIPTION = 'expo/1.1.0 - sprint26';
     console.log('NAME: ' + config.name); // prints 'My App'
     console.log('COMBO_version: ' + COMBO_version);
     return {
